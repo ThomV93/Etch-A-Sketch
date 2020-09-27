9ca99e89cca365df75455be25a42545619927b95
Thomas Veit <thomas.veit93@gmail.com>
2020-09-27 19:56:37 +0100

    grid btns event listeners added

--- Code/code.js
+++ Code/code.js
@@ -7,7 +7,7 @@ const thirtyTwo_btn = document.getElementById("32x");
 const sixtyFour_btn = document.getElementById("64x");
 const gridContainer_div = document.getElementById("grid-container");
 
-//create a grid with default 16x16 divs
+//create a dynamic grid based on the user choice
 function createGrid(rows, cols) {
     gridContainer_div.style.setProperty("--grid-rows", rows);
     gridContainer_div.style.setProperty("--grid-cols", cols);
@@ -18,7 +18,21 @@ function createGrid(rows, cols) {
 };
 //adds event listeners fot the color buttons
 //adds event listeners for the grid buttons
+function gridBtns() {
+    sixteen_btn.addEventListener("click", function(){
+        createGrid(16, 16);
+    });
 
+    thirtyTwo_btn.addEventListener("click", function(){
+        createGrid(32, 32);
+    });
+
+    sixtyFour_btn.addEventListener("click", function(){
+        createGrid(64, 64);
+    });
+};
+
+gridBtns();
 //grid can have different number of divs in the same ammount of pixels
 //Set up a hover effect so that the grid divs change color when your mouse passes over
 //add a new class to the div in order to changes it's color
