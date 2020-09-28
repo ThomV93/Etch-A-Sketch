581f20778e15bf221a8990eae2e4629e9927b991
Thomas Veit <thomas.veit93@gmail.com>
2020-09-28 22:31:43 +0100

    reset button

--- Code/README.md
+++ Code/README.md
@@ -1,1 +1,3 @@
 # Etch-A-Sketch
+
+Assignment from The Odin Project using HTML/ CSS and Vanilla JS
--- Code/code.js
+++ Code/code.js
@@ -6,6 +6,8 @@ const sixteen_btn = document.getElementById("16x");
 const thirtyTwo_btn = document.getElementById("32x");
 const sixtyFour_btn = document.getElementById("64x");
 const gridContainer_div = document.getElementById("grid-container");
+const btnsContainer_div = document.getElementById("btns-container");
+let gridItem_div = document.getElementsByClassName("grid-item");
 
 //create a dynamic grid based on the user choice
 function createGrid(rows, cols) {
@@ -16,24 +18,41 @@ function createGrid(rows, cols) {
         gridContainer_div.appendChild(cell).className = "grid-item";
     };
 };
+
 //adds event listeners fot the color buttons
+function colorBtns() {
+    black_btn.addEventListener("click", changeColor(black));
+}
 //adds event listeners for the grid buttons
 function gridBtns() {
-    sixteen_btn.addEventListener("click", function(){
+    sixteen_btn.addEventListener("click", function() {
         createGrid(16, 16);
     });
 
-    thirtyTwo_btn.addEventListener("click", function(){
+    thirtyTwo_btn.addEventListener("click", function() {
         createGrid(32, 32);
     });
 
-    sixtyFour_btn.addEventListener("click", function(){
+    sixtyFour_btn.addEventListener("click", function() {
         createGrid(64, 64);
     });
 };
 
 gridBtns();
-//grid can have different number of divs in the same ammount of pixels
 //Set up a hover effect so that the grid divs change color when your mouse passes over
 //add a new class to the div in order to changes it's color
-//create a reset button to clear the grid
+
+//refresh window
+function refreshGame() {
+    window.location.reload();
+}
+
+//Button to reset the game refreshing the browser window
+function replayButton() {
+    let replayBtn = document.createElement("button");
+    replayBtn.innerHTML = "Reset"
+    btnsContainer_div.appendChild(replayBtn);
+    replayBtn.addEventListener("click", refreshGame);
+}
+
+replayButton();
